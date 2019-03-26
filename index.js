26c62fdc2429fbe67c0352e377919093ef61c4d1
AJ Jarzebiak <asjarzeb@gmail.com>
2019-03-26 00:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const dodger = document.getElementById('dodger')
 
 function onKeyDown(event) {
   if (event.keyCode === 37) {
+    // how do we actually move this?
+    
+    console.log(dodger.style.left)
     
   }
 }
